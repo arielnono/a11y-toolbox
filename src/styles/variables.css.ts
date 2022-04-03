0a9dc52ea7d72f5d1eb2b4bc4f2b26fe8e8c8972
arieln <nono.ariel@yahoo.it>
2022-04-03 10:04:39 +0200

    draft: add mixins withous sass

--- src/styles/variables.css.ts
+++ src/styles/variables.css.ts
@@ -5,5 +5,6 @@ export const variables = css`
     --headings-font-family: 'merriweather-bold', Georgia, 'Times New Roman',
       serif;
     --body-font-family: 'Atkinson-regular', helvetica, Verdana, sans-serif;
+    --base-font-size: 1rem;
   }
 `;
